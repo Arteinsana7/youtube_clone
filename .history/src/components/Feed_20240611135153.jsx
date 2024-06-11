3e65568ebc776979b992054cfd6ba5a85afefcf3
Arteinsana7 <147487242+Arteinsana7@users.noreply.github.com>
2024-06-11 14:23:17 -0400

    Update side Bar  📐
    Update sideBar and implementing API call from Rapid API

--- .history/src/components/Feed_20240611135153.jsx
+++ .history/src/components/Feed_20240611135153.jsx
@@ -1,9 +1,8 @@
-import { useState, useEffect} from 'react';
-import { Box, Stack, Typography  } from '@mui/material';
-import {SideBar, Videos} from './'
- 
-import { fetchFromAPI } from '../utils/fetchFromAPI';
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
@@ -19,10 +18,10 @@ const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2024 Luz Media
+          Copyright © 2022 JSM Media
         </Typography>
       </Box>
 
--- .history/src/components/Feed_20240611135239.jsx
+++ .history/src/components/Feed_20240611135239.jsx
@@ -19,10 +19,10 @@ const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2024 Luz Media
+          Copyright © 2022 JSM Media
         </Typography>
       </Box>
 
